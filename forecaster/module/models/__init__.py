d819d18471b8d4be0fb737ca08bc5c80b0a8f7ef
Etienne <etienne.micheladj@gmail.com>
2024-02-29 23:39:13 +0100

    feat(forecaster): reverse odd model

--- forecaster/module/models/__init__.py
+++ forecaster/module/models/__init__.py
@@ -1,1 +1,2 @@
 from .dummy import *
+from .reverse_odd import *
---
+++ forecaster/module/models/reverse_odd.py
@@ -0,0 +1,35 @@
+import random
+import uuid
+import json
+import time
+from module.models.module import Module
+import time
+
+class ReverseOdd(Module):
+    def __init__(self, config):
+        Module.__init__(self, config)
+
+    def get_pred(self, match_info):
+        probs = [1/odd for odd in match_info["Odds"]]
+        probs = [p/sum(probs) for p in probs]
+        return {"classic_preds":probs, "other_preds":[]}
+
+    def act(self, data):
+        """
+        input: {matchs_infos, winners}
+        DO NOT USE WINNERS variable ;)
+
+        output: {matchs_infos, preds, winners}
+        preds: [{classic_preds:[0.4, 0.6], other_preds:[]}]
+        """
+        preds = []
+        for match_info in data["matchs_infos"]:
+            pred = self.get_pred(match_info)
+            preds.append(pred)
+
+        data["preds"] = preds
+
+        return data
+
+
+
